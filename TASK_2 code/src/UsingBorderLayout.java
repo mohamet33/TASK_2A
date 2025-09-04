a3636e7056617bea7fda1d83418c01a8b75a8665
unknown <Yuusuflaki82@gmail.com>
2025-09-04 14:47:04 +0300

    commit 1
    ALi pogpa

--- TASK_2 code/src/UsingBorderLayout.java
+++ TASK_2 code/src/UsingBorderLayout.java
@@ -4,5 +4,16 @@ public class UsingBorderLayout {
     public UsingBorderLayout() {
         JFrame frame = new JFrame("Border Layout Frame");
         frame.setLayout(new BorderLayout());
+<<<<<<< Updated upstream
+=======
+        JLabel title = new JLabel("My Music Genre", SwingConstants.CENTER);
+        title.setFont(new Font("Serif", Font.BOLD, 22));
+        frame.add(title, BorderLayout.NORTH);
+        JButton rapButton = new JButton("Rap Music");
+        frame.add(rapButton, BorderLayout.SOUTH);
+        JButton gospelButton = new JButton("Gospel Music");
+        frame.add(gospelButton, BorderLayout.WEST);
+
+>>>>>>> Stashed changes
     }
 }
