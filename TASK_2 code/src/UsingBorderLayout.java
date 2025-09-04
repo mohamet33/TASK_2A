8c85022839444ced4dae89d98cb3c393787c6075
unknown <Yuusuflaki82@gmail.com>
2025-09-04 14:37:05 +0300

    commit 2
    wan hubinaynaa

--- TASK_2 code/src/UsingBorderLayout.java
+++ TASK_2 code/src/UsingBorderLayout.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
 public class UsingBorderLayout {
+    public UsingBorderLayout() {
+        JFrame frame = new JFrame("Border Layout Frame");
+        frame.setLayout(new BorderLayout());
+    }
 }
